be3c50e33414f58a8b386b3082db479beec3f092
lgants <lgants@gmail.com>
2017-10-12 15:50:58 -0500

    added mutations

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 // by requiring `babel/register` all successive `require`s will be Babel'd
-require('babel-core/register.js');
+require('babel-core/register');
 require('./server.js');
 
-var express = require('express')
-// server.js
-// import express from 'express';
+import express from 'express';
+import schema from './schema';
+// new dependencies
+import { graphql } from 'graphql';
+import bodyParser from 'body-parser';
 
 let app  = express();
 let PORT = 3000;
 
+// parse POST body as text
+// enforcing new content type is a good pattern for servers that mix GraphQL with REST
+app.use(bodyParser.text({ type: 'application/graphql' }));
+
 app.post('/graphql', (req, res) => {
-  res.send('Hello!');
+  // execute GraphQL
+  graphql(schema, req.body)
+  .then((result) => {
+    res.send(JSON.stringify(result, null, 2));
+  });
 });
 
 let server = app.listen(PORT, function () {
@@ -19,3 +29,10 @@ let server = app.listen(PORT, function () {
 
   console.log('GraphQL listening at http://%s:%s', host, port);
 });
+
+// curl -XPOST -H "Content-Type:application/graphql"  -d 'query RootQueryType { count }' http://localhost:3000/graphql
+// ^can omit query type:
+// curl -XPOST -H 'Content-Type:application/graphql'  -d '{ count }' http://localhost:3000/graphql
+
+// mutation 
+// curl -XPOST -H 'Content-Type:application/graphql' -d 'mutation RootMutationType { updateCount }' http://localhost:3000/graphql
--- schema.js
+++ schema.js
@@ -3,17 +3,32 @@ import {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLInt
-} from 'graphql/lib/type';
+} from 'graphql';
 
 let count = 0;
 
+// top-level query object returns a `RootQueryType` object, which has a `count` field, which is an integer.
 let schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
       count: {
         type: GraphQLInt,
+        description: 'The count!',
         resolve: function() {
+          return count;
+        }
+      }
+    }
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'RootMutationType',
+    fields: {
+      updateCount: {
+        type: GraphQLInt,
+        description: 'Updates the count',
+        resolve: function() {
+          count += 1;
           return count;
         }
       }
